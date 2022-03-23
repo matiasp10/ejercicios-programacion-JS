6bf4ac29c761c06793f384c2738131303a610529
Matias E. Petenatti <matiasodin96@gmail.com>
2022-03-23 18:15:57 -0300

    ejercicio 8

---
+++ 08-Objetos-acumulador/08-Objetos-acumulador.js
@@ -0,0 +1,36 @@
+// Crear una función constructor Accumulator(startingValue).
+
+// El objeto que crea debería:
+
+// Almacene el “valor actual” en la propiedad value. El valor inicial se establece en el argumento del constructor startingValue.
+// El método read() debe usar prompt para leer un nuevo número y agregarlo a value.
+// En otras palabras, la propiedad value es la suma de todos los valores ingresados por el usuario con el valor inicial startingValue.
+
+// Aquí está la demostración del código:
+
+/*
+
+ let accumulator = new Accumulator(1); // valor inicial 1
+
+ accumulator.read(); // agrega el valor introducido por el usuario
+ accumulator.read(); // agrega el valor introducido por el usuario
+
+ alert(accumulator.value); // muestra la suma de estos valores
+
+*/
+
+function Accumulator(startingValue) {
+    this.value = startingValue
+    read = function read() {
+        let num = +prompt("Que valor?",0)
+        this.value = num
+    }
+}
+
+let accumulator = new Accumulator(1)
+
+accumulator.read()
+accumulator.read()
+
+console.log(accumulator.value);
+
